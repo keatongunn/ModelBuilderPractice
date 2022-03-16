f72773ec73fdbd2784f5bea297a979037b50b1a4
Keaton Gunn <keatongunn@hotmail.com>
2022-03-16 13:01:13 -0500

    Add project files.

---
+++ ModelBuilderPractice/Models/Journal.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelBuilderPractice.Models
+{
+    public class Journal
+    {
+        public int JournalNumber { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ ModelBuilderPractice/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBuilderPractice.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Journal> Journals { get; set; }
+
+        public User()
+        {
+            Journals = new HashSet<Journal>();
+        }
+    }
+}
